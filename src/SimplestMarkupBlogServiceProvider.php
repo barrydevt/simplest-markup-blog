b5dbffb7638700cea0ec7babfae458ada66766a3
Barry Mason <barrywebla@gmail.com>
2023-03-01 20:22:24 +0000

    Wip

---
+++ src/SimplestMarkupBlogServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Barrydevt\SimplestMarkupBlog;
+
+use Illuminate\Support\ServiceProvider;
+
+class SimplestMarkupBlogServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        # code...
+    }
+
+    public function register()
+    {
+        # code...
+    }
+}
